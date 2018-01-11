d1aac95a7a859e67fb8d15ffd20d52ff9c89368b
vv <na@test.net>
2018-01-11 09:17:10 -0500

    added a default router and added a test link to step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { Step3Component } from './components/step3/step3.component';
 import { Step4Component } from './components/step4/step4.component';
 
 const routes: Routes = [
+    { path: '', redirectTo: '/step1', pathMatch: 'full' },
     { path: 'step2', component: Step2Component },
     { path: 'step1', component: Step1Component },
     { path: 'step3', component: Step3Component },
