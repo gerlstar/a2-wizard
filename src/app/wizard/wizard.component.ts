e71fb407f3e761383d69ef58609f71a728e61a4b
vv <na@test.net>
2018-01-11 16:25:24 -0500

    added css for active class in the wizard

--- src/app/wizard/wizard.component.ts
+++ src/app/wizard/wizard.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterContentInit} from '@angular/core';
+import {RoutesService} from "./../services/routes.service";
+
 
 @Component({
-  selector: 'app-wizard',
-  templateUrl: './wizard.component.html',
-  styleUrls: ['./wizard.component.css']
+    selector: 'app-wizard',
+    templateUrl: './wizard.component.html',
+    styleUrls: ['./wizard.component.css']
 })
-export class WizardComponent implements OnInit {
+export class WizardComponent implements OnInit, AfterContentInit {
+    private style1: boolean;
+    numSteps: Array<number> = [1, 2, 3, 4, 5];
 
-  numSteps: Array<number> = [1,2,3,4,5];
-  constructor() {
+    constructor(private routesService: RoutesService) {
+        this.style1 = true;
+    }
 
-  }
+    ngOnInit() {
+        console.log('wizard...');
+        // console.log(this.routesService);
+        this.routesService.getRoutes()
+            .subscribe(res => {
+                console.log(this.routesService.routes)
+            })
+    }
 
-  ngOnInit() {
-  }
+    ngAfterContentInit(){
+        console.info('routes =',this.routesService.routes);
+    }
 
 }
